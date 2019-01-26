1ae92be5f0a360f6415084a310a0068d280cb11b
Walkplay <ivshar@ukr.net>
2019-01-27 00:32:06 +0200

    Create labirint on desk

--- AStar/main.c
+++ AStar/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "Desklib.h"
+
+#define WIDTH 49
+#define HEIGHT 49
+
 
 int main() {
-    printf("Hello, World!\n");
+    struct args params;
+    char arr[HEIGHT][WIDTH];
+    params.arr_ptr = &arr[0][0];
+    params.height = HEIGHT;
+    params.width = WIDTH;
+    CreateDeskOfZeros(params);
+    CreateLabirint(params,1);
+    ShowDesk(params);
+
     return 0;
 }
+
